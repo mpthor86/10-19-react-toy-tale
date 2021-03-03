bbd999f16870c7bae73be94e87269a49f9bc1723
michael <mpthornton86@gmail.com>
2021-03-03 14:50:51 -0700

    pod-3-3

--- src/ToyCard.js
+++ src/ToyCard.js
@@ -2,12 +2,16 @@
 
 function ToyCard(props){
     // const {id, image, name} = props.toy // destructuring is an option
+    function handleClick(){
+        props.delete(props.id)
+    }
     return(
         <div class="card" id={`toy-${props.id}`}>
             <h2>{props.name}</h2>
             <img src={props.image} class="toy-avatar"/>
             <p>{props.likes} Likes </p>
-            <button class="like-btn">Like &lt;3</button>
+            <button onClick={() => props.addLikes(props.id, props.likes)} class="like-btn">Like &lt;3</button>
+            <button onClick={handleClick}>Delete</button>
         </div>
     )
 }
--- src/ToysContainer.js
+++ src/ToysContainer.js
@@ -15,6 +15,38 @@ class ToysContainer extends React.Component{
         whatever: "hello"
     }
 
+    delete = (id) => {
+        this.setState((prevState) => {
+            return {
+              toys: prevState.toys.filter(toy => {return toy.id !== id})
+            }
+        })
+    }
+
+    addLikes = (id, likes) =>{
+        const configObj = {
+            method: 'PATCH',
+            headers: {
+             "Content-Type": "application/json",
+             "Accept": "application/json"
+            },
+            body: JSON.stringify({likes: likes + 1}) 
+        }
+
+        fetch(`http://localhost:3000/toys/${id}`, configObj)
+        .then(resp => resp.json())
+        .then(json => this.updateToy(json))
+    }
+
+    updateToy = (json) => {
+        this.setState((prevState) => {
+            const indexed = prevState.toys.findIndex(toy => toy.id === json.id)
+            return {toys: [...prevState.toys.slice(0, indexed), //toys before
+                json, //updated toy
+                ...prevState.toys.slice(indexed + 1)]} //toys after
+        })
+    }
+
     makeToyCards(){
         //utilize STATE
         let displayedToys = this.state.toys
@@ -24,7 +56,7 @@ class ToysContainer extends React.Component{
             toy.name.toLowerCase().includes(this.state.search.toLowerCase()))
         }
 
-        return displayedToys.map(toy => <ToyCard toy={toy} id={toy.id} name={toy.name} image={toy.image} likes={toy.likes} />)
+        return displayedToys.map(toy => <ToyCard addLikes={this.addLikes} delete={this.delete} toy={toy} id={toy.id} name={toy.name} image={toy.image} likes={toy.likes} />)
     }
 
     // componentDidUpdate(){
